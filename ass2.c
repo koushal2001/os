2b1542ff44d1ce87276015645b7e0e81be8acb38
Koushal Bhat <60033544+koushal2001@users.noreply.github.com>
2021-12-16 10:49:10 +0530

    Update ass2.c

--- ass2.c
+++ ass2.c
@@ -6,6 +6,7 @@
 #define SIZE 100
 int arr[SIZE],n;
 
+
 void print_arr(int arr[], int n)
 {
 	int i;
@@ -85,9 +86,11 @@ void bubble_sort(int arr[], int n)
      
     for (j = 0; j < n-i-1; j++)
         if (arr[j] > arr[j+1])
+        {
             temp=arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
+        }
 }
  
 
@@ -132,6 +135,8 @@ int main()
 		printf("Enter the element [%d]:- ", i);
 		scanf("%d", &arr[i]);
 	}
+    bubble_sort(arr,n);
+    print_arr(arr,n);
     forkEx();
     return 0;
 }
